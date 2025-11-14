6791c1cb7815e5006da748be333f27ea63b0d6ab
Gabriel23Clases <garcia.lobo.gabriel@iescamas.es>
2025-11-14 18:40:35 +0100

    3enRaya a completar

--- src/tresEnRaya/tresEnRaya1.java
+++ src/tresEnRaya/tresEnRaya1.java
@@ -1,10 +1,15 @@
 package tresEnRaya;
 
+import java.util.Arrays;
+
 public class tresEnRaya1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		char jugador1 = 'o';
+		char jugador2 = 'x';
+		char vacio = 'v';
+		char [] [] tablero = {{'v','v','v'}, {'v','v','v'},{'v','v','v'}};
+		System.out.println(Arrays.deepToString (tablero));
 	}
 
 }
